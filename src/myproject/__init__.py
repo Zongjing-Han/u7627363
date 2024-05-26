33b37acd5515394a9ac55f04db6f06678092f308
Zongjing-Han <141532506+Zongjing-Han@users.noreply.github.com>
2024-05-26 23:05:48 +1000

    Update __init__.py

--- src/myproject/__init__.py
+++ src/myproject/__init__.py
@@ -1,10 +1,10 @@
 """my doctsring
 """
-__author__ = "YOUR NAME"
-__copyright__ = "Copyright 2016-2021, YOUR NAME"
-__credits__ = ["YOUR NAME"]
+__author__ = "Zongjing Han"
+__copyright__ = "Copyright 2016-2021, Zongjing Han"
+__credits__ = ["Zongjing Han"]
 __license__ = "BSD"
 __version__ = "2020.6.5"  # A DATE BASED VERSION
-__maintainer__ = "YOUR NAME"
-__email__ = "YOUR@email"
+__maintainer__ = "Zongjing Han"
+__email__ = "u7627363@anu.edu.au"
 __status__ = "alpha"
