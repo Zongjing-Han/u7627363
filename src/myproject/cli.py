464a50de2dfd47c7aff5e5088588a5593c046ef6
Zongjing-Han <141532506+Zongjing-Han@users.noreply.github.com>
2024-05-26 23:06:03 +1000

    Update cli.py

--- src/myproject/cli.py
+++ src/myproject/cli.py
@@ -4,14 +4,16 @@ import click
 
 from scitrack import CachingLogger
 
+from cogent3 import make_unaligned_seqs
+from cogent3.core.alignment import SequenceCollection
 
-__author__ = "YOUR NAME"
-__copyright__ = "Copyright 2016-2021, YOUR NAME"
-__credits__ = ["YOUR NAME"]
+__author__ = "Zongjing Han"
+__copyright__ = "Copyright 2016-2021, Zongjing Han"
+__credits__ = ["Zongjing Han"]
 __license__ = "BSD"
 __version__ = "2020.6.5"  # A DATE BASED VERSION
-__maintainer__ = "YOUR NAME"
-__email__ = "YOUR@email"
+__maintainer__ = "Zongjing Han"
+__email__ = "u7627363@anu.edu.au"
 __status__ = "alpha"
 
 
@@ -23,6 +25,81 @@ LOGGER = CachingLogger()
 def main():
     """docstring explains what your cl app does"""
     pass
+
+def unique_kmers(seqs: SequenceCollection, k: int = 2) -> dict[str, set[str]]:
+    """returns a {seqname: set(str, ...)}"""
+    # Dictionary to store k-mers and the sequences they appear in
+    kmer_dict = {}
+    
+    # Iterate over sequences and extract k-mers
+    for s in seqs.seqs:
+        name = s.name
+        sequence = str(s)  # Access the sequence data correctly
+        for i in range(len(sequence) - k + 1):
+            kmer = sequence[i:i + k]
+            if kmer not in kmer_dict:
+                kmer_dict[kmer] = set()
+            kmer_dict[kmer].add(name)
+
+    unique_kmers_dict = {}
+    for kmer, names in kmer_dict.items():
+        if len(names) == 1:  
+            name = next(iter(names))
+            if name not in unique_kmers_dict:
+                unique_kmers_dict[name] = set()
+            unique_kmers_dict[name].add(kmer)
+    
+    return unique_kmers_dict
+
+@main.command(no_args_is_help=True)
+@click.option(
+    "-i",
+    "--infile",
+    required=True,
+    type=click.Path(exists=True),
+    help="Path to the input file containing sequences"
+)
+@click.option(
+    "-k",
+    "--kmer_length",
+    required=True,
+    type=int,
+    help="Length of the k-mers"
+)
+@click.option(
+    "-o",
+    "--outfile",
+    required=True,
+    type=click.Path(),
+    help="Path to the output file"
+)
+def find_unique_kmers(infile, kmer_length, outfile):
+    """Identify unique k-mers from sequences"""
+    with open(infile, 'r') as f:
+        seqs_dict = {}
+        seq_id = None
+        sequence = []
+        for line in f:
+            line = line.strip()
+            if line.startswith(">"):
+                if seq_id is not None:
+                    seqs_dict[seq_id] = "".join(sequence)
+                seq_id = line[1:]  # Get rid of '>'
+                sequence = []
+            else:
+                sequence.append(line)
+        if seq_id is not None:  # For the last sequence
+            seqs_dict[seq_id] = "".join(sequence)
+
+    seqs = SequenceCollection(seqs_dict, moltype="dna")
+    unique_kmers_dict = unique_kmers(seqs, kmer_length)
+
+    with open(outfile, 'w') as f:
+        for seq_id, kmers in unique_kmers_dict.items():
+            f.write(f"{seq_id}\t{','.join(kmers)}\n")
+
+    click.echo(f"Unique k-mers written to {outfile}")
+
 
 
 # note that I often define reusable options in a central place
